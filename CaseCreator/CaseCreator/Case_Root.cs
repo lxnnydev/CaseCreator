fee822a962cf15a357dc33f78314dfb89b507f25
lxnnydev <89148963+lxnnydev@users.noreply.github.com>
2022-10-24 23:13:28 +0530

    Add files via upload

---
+++ CaseCreator/CaseCreator/Case_Root.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseCreator
+{
+    public class Item
+    {
+        public string name { get; set; }
+        public string image { get; set; }
+        public int value { get; set; }
+        public int chance { get; set; }
+    }
+
+    public class Case_Root
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+        public List<Item> items { get; set; }
+    }
+
+
+}
